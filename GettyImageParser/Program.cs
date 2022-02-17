f65e52eb3f30d63cbcf1240c562d019da227c07d
dima <testImplementation>
2022-02-17 23:59:25 +0500

    create getty img object

--- GettyImageParser/Program.cs
+++ GettyImageParser/Program.cs
@@ -2,52 +2,18 @@
 
 // Load the document using HTMLAgilityPack as normal
 
-using Fizzler.Systems.HtmlAgilityPack;
-using HtmlAgilityPack;
+using GettyImageParser.model;
 
-var html = new HtmlDocument();
-html.LoadHtml(@"
-  <html>
-      <head>
-        <meta content='Getty Images' property='og:site_name'>
-        <meta content='Russian Footage' itemprop='creditText' />
-          <meta
-        content='https://media.gettyimages.com/videos/an-aerial-view-of-a-moscow-cityscape-in-winter-day-video-id1303150754?s=170x170'
-    property='og:image' />                                           </head>
-      <body>
-        <div>
-          <p class='content'>Fizzler</p>
-          <p>CSS Selector Engine</p></div>
-      </body>
-  </html>");
+var getty = new GettyImage();
 
-// Fizzler for HtmlAgilityPack is implemented as the
-// QuerySelectorAll extension method on HtmlNode
+var obj = getty.GetData("1303150754");
 
-var document = html.DocumentNode;
 
-// yields: [<p class="content">Fizzler</p>]
-document.QuerySelectorAll(".content");
+  
+Console.WriteLine(obj.author);
+Console.WriteLine(obj.duration);
+Console.WriteLine(obj.name);
+Console.WriteLine(obj.companyName);
+Console.WriteLine(obj.preview);
+Console.WriteLine(obj.imageByteArray.Length);
 
-// yields: [<p class="content">Fizzler</p>,<p>CSS Selector Engine</p>]
-document.QuerySelectorAll("p");
-
-// yields empty sequence
-document.QuerySelectorAll("body>p");
-
-// yields [<p class="content">Fizzler</p>,<p>CSS Selector Engine</p>]
-document.QuerySelectorAll("body p");
-
-// yields [<p class="content">Fizzler</p>]
-document.QuerySelectorAll("p:first-child");
-
-var siteNameNode = document.SelectSingleNode("//meta[@property='og:site_name']");
-var name = siteNameNode.Attributes["content"].Value;
-Console.WriteLine(name);
-var companyNameNode = document.SelectSingleNode("//meta[@itemprop='creditText']");
-var companyName = companyNameNode.Attributes["content"].Value;
-Console.WriteLine(companyName);
-
-var previewLinkNode = document.SelectSingleNode("//meta[@property='og:image']");
-var preview = previewLinkNode.Attributes["content"].Value;
-Console.WriteLine(preview);
---
+++ GettyImageParser/model/GettyImage.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using HtmlAgilityPack;
+
+namespace GettyImageParser.model;
+
+public class GettyImage
+{
+    public string author { get; set; }
+    public string name { get; set; }
+    public string companyName { get; set; }
+    public string preview { get; set; }
+    public string duration { get; set; }
+    public byte[] imageByteArray { get; set; }
+
+    public GettyImage GetData(string code)
+    {
+        var url = $"https://www.gettyimages.com/search/2/image?phrase={code}";
+        var uri = new Uri(url);
+        var html = getHtml(uri);
+        var obj = parseHtml(html);
+        return obj;
+    }
+    
+
+    private string getHtml(Uri url)
+    {
+        using (WebClient client = new WebClient())
+        {
+            string htmlCode =
+                client.DownloadString(url);
+            return htmlCode;
+        }
+    }
+
+    private GettyImage parseHtml(string htmlCode)
+    {
+        var html = new HtmlDocument();
+        html.LoadHtml(htmlCode);
+        var document = html.DocumentNode;
+  
+
+        var authorNode = document.SelectSingleNode("//meta[@itemprop='author']");
+        author = authorNode.Attributes["content"].Value;
+  
+        var siteNameNode = document.SelectSingleNode("//meta[@property='og:site_name']");
+        name = siteNameNode.Attributes["content"].Value;
+  
+        var companyNameNode = document.SelectSingleNode("//meta[@itemprop='creditText']");
+        companyName = companyNameNode.Attributes["content"].Value;
+
+        var previewLinkNode = document.SelectSingleNode("//meta[@property='og:image']");
+        preview = previewLinkNode.Attributes["content"].Value;
+  
+        var durationNode = document.SelectSingleNode("//div[@class='asset-detail asset-detail--location']");
+        duration = durationNode.PreviousSibling.InnerText;
+  
+    
+        using (var webClient = new WebClient())
+        {
+            imageByteArray = webClient.DownloadData(preview);
+        }
+
+        return this;
+    }
+}
